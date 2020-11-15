961f996b1ef13bec05940800e5bf64515cc3d96c
CodeXplore <codexplore.channel@gmail.com>
2020-11-15 18:32:03 +0800

    Update 1290_convert_Binary_to_Integer.js

--- 1290_convert_Binary_to_Integer.js
+++ 1290_convert_Binary_to_Integer.js
@@ -1,6 +1,8 @@
 var getDecimalValue = function(head) {
     let num = 0;
     while(head){
+        //(num << 1) => Left shift num by 1 position to make way for next bit
+        //(num << 1) | head.val => Add next bit to num at least significant position
         num = (num << 1) | head.val;
         head = head.next
     }
