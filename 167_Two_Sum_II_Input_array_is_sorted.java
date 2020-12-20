40ec8a058891dae1d276ad91258643893c19c608
CodeXplore <codexplore.channel@gmail.com>
2020-12-20 22:36:18 +0800

    Create 167_Two_Sum_II_Input_array_is_sorted.java

---
+++ 167_Two_Sum_II_Input_array_is_sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length-1;
+        while (i < j) {
+            if ((numbers[i] + numbers[j]) > target)
+                j--;
+            else if ((numbers[i] + numbers[j]) < target)
+                i++;
+            else 
+                return new int[] {i+1,j+1};
+        }
+        
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
