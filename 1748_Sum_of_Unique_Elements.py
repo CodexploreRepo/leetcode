d8ef2cb97a3f8a81f9dd7149e7ba322ca021033a
CodeXplore <codexplore.channel@gmail.com>
2021-02-24 07:32:25 +0800

    Create 1748_Sum_of_Unique_Elements.py

---
+++ 1748_Sum_of_Unique_Elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        counts = collections.Counter(nums)
+
+        return sum([key for key, value in counts.items() if value == 1])
+                
+            
+            
+                
+        
