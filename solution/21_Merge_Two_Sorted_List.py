d781337d7105404a9b34855ff0431522948a06e0
CodeXplore <codexplore.channel@gmail.com>
2021-08-04 07:19:36 +0800

    Update 21_Merge_Two_Sorted_List.py

--- solution/21_Merge_Two_Sorted_List.py
+++ solution/21_Merge_Two_Sorted_List.py
@@ -5,22 +5,17 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        #Head to keep track the head of the result linked list
-        #Tail to keep track the end of the result linked list
-        head = tail = ListNode(-1)
-        while(l1 and l2):
-            #print(l1.val, l2.val)
-            if (l1.val > l2.val):
-                tail.next = l2
-                l2 = l2.next
+        #dummy_head to keep track the head of the result linked list
+        #tail to keep track the end of the result linked list
+        dummy_head = tail = ListNode()
+        while (l1 and l2):
+            if l1.val < l2.val:
+                tail.next, l1 = l1, l1.next
             else:
-                tail.next = l1
-                l1 = l1.next
+                tail.next, l2 = l2, l2.next
             tail = tail.next
-         
         
         if l1: tail.next = l1
         if l2: tail.next = l2
         
-        
-        return head.next
+        return dummy_head.next #as the init head.val = 0
