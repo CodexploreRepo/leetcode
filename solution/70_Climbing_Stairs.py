2198c5231c186cc51a001c131f24c53d719682b2
CodeXplore <codexplore.channel@gmail.com>
2021-09-29 21:56:17 +0800

    Update 70_Climbing_Stairs.py

--- solution/70_Climbing_Stairs.py
+++ solution/70_Climbing_Stairs.py
@@ -1,7 +1,8 @@
 class Solution:
-    memo = {1: 1, 2:2} #T(1) = 1, i.e: only 1 way to climb up the stair = 1, and T(2) = 2 as 2 ways to climb up the stair = 2
-    def climbStairs(self, n: int) -> int:
-        if n not in self.memo: 
-            #@ T(n): first step = 1, remaining steps = T(n-1) or first step = 2, remaing steps = T(n-2)
-            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2) 
-        return self.memo[n]
+    def climbStairs(self, n: int, memo = {1: 1, 2:2}) -> int:
+        if n not in memo:
+            #T(n) = T(n-1) + T(n-2)
+            memo[n] = self.climbStairs(n-1, memo) + self.climbStairs(n-2, memo) 
+        return memo[n]
+
+
