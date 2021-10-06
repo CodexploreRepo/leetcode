fbe3a386e818214e418c52fec54238789ba28b64
CodeXplore <codexplore.channel@gmail.com>
2021-10-06 16:22:54 +0800

    Update 622_Design_Circular_Queue.py

--- solution/622_Design_Circular_Queue.py
+++ solution/622_Design_Circular_Queue.py
@@ -2,8 +2,7 @@ class MyCircularQueue:
 
     def __init__(self, k: int):
         self.queue = [None]*k
-        self.cur_size = 0
-        self.max_size = k
+        self.size = k
         self.head = self.tail = -1
         
     def enQueue(self, value: int) -> bool:
@@ -16,13 +15,10 @@ class MyCircularQueue:
             else:
                 #if queue is not empty, just increase self.tail by 1
                 #if self.tail == self.max_size, so (% self.max_size) to circular back to 0 pos
-                self.tail = (self.tail + 1)%self.max_size
+                self.tail = (self.tail + 1)%self.size
             #store the "value" in tail cell
             self.queue[self.tail] = value
-            #update the cur_size by 1
-            self.cur_size += 1
             return True #return True if can enqueue
-
 
     def deQueue(self) -> bool:
         #Dequeue: To take care of the head
@@ -32,10 +28,8 @@ class MyCircularQueue:
             if self.head == self.tail: #in case, only left with 1 element
                 self.head = self.tail = -1 #init both head, tail to -1
             else:
-                self.queue[self.head] = None #change the value @ head to None
-                self.head = (self.head + 1)%self.max_size #increase head by 1
+                self.head = (self.head + 1)%self.size #increase head by 1
                 #if self.head == self.max_size, so (% self.max_size) to circular head back to 0 pos
-            self.cur_size -= 1
             return True
             
     def Front(self) -> int:
@@ -45,8 +39,8 @@ class MyCircularQueue:
         return self.queue[self.tail] if self.tail >= 0 else -1
 
     def isEmpty(self) -> bool:
-        return self.cur_size == 0
+        return self.head == -1 #either head or tail = -1 
         
-
     def isFull(self) -> bool:
-        return self.cur_size == self.max_size
+        #Need to see the pos of head and tail when the queue is full
+        return ((self.tail + 1) % self.size) == self.head
