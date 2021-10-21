440c6f6324fb219a84026d44635899d5d728a2df
CodeXplore <codexplore.channel@gmail.com>
2021-10-21 17:59:33 +0800

    Create 429_N-ary_Tree_Level_Order_Traversal.py

---
+++ solution/429_N-ary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,17 @@
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root: return []
+        queue = collections.deque([root])
+        res = []
+        while(queue):
+            size, cur_level = len(queue), []
+            
+            for _ in range(size):
+                cur_node = queue.popleft()
+                cur_level.append(cur_node.val)
+                for child in cur_node.children:
+                    if child:
+                        queue.append(child)
+            res.append(cur_level)
+        
+        return res
