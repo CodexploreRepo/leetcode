5be441e3207ec8ede4ffb29fbacfa8b7680e9e68
CodeXplore <codexplore.channel@gmail.com>
2021-12-20 23:24:49 +0700

    Update 198_House_Robber.py

--- solution/198_House_Robber.py
+++ solution/198_House_Robber.py
@@ -1,10 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0]*(n)
-        dp[n-1] = nums[n-1]
-        dp[n-2] = nums[n-2]
-        for i in range(n-3, -1, -1):
-            for j in range(i, n-2):
-                dp[i] = max(dp[i], nums[i] + dp[j+2])
-        return max(dp)
+        if n == 1: #In case, only 1 house
+            return nums[0]
+        dp = [0]*n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i - 2] + nums[i])
+        return dp[-1]
+
+        
