f5219fbf3e4e0ac03de721cada3e8cd44ff5cc22
CodeXplore <codexplore.channel@gmail.com>
2022-01-27 13:28:07 +0800

    Update 160_Intersection_of_Two_Linked_Lists.py

--- solution/160_Intersection_of_Two_Linked_Lists.py
+++ solution/160_Intersection_of_Two_Linked_Lists.py
@@ -1,34 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
-        curA, curB = headA, headB
-        lenA, lenB = 1,1
-        
-        #Step 1: Traverse to the end of each linked list, and calculate the branch length
-        while (curA.next):
-            curA = curA.next
-            lenA+=1
-        while(curB.next):
-            curB = curB.next
-            lenB+=1
-        #Step 2: If 2 last node of branch A, branch B is same (i.e: they are intersect with each other)
-        
-        if curA == curB:
-            #Step 2.1: Compare the branch length. If branch A > branch B, then we need to advance branch A by diff
-            #Where diff = lenA - lenB. Once advanced, So both A and B will start at the same place, and just try to see when they meet each other
-            #That will be the intersection point
-            if lenA > lenB:
-                diff = lenA - lenB
-                for _ in range(diff):
-                    headA = headA.next
-            else:
-                diff = lenB - lenA
-                for _ in range(diff):
-                    headB = headB.next
-            #Once advanced, So both A and B will start at the same place, and just try to see when they meet each other
-            #That will be the intersection point
-            while (headA != headB and headA and headB):
+    def getLinkedListLength(self, head):
+        ll_length = 0
+        while(head):
+            ll_length +=1
+            head = head.next
+        return ll_length
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        lenA = self.getLinkedListLength(headA)
+        lenB = self.getLinkedListLength(headB)
+        if lenA > lenB:
+            for _ in range(lenA-lenB):
                 headA = headA.next
+        else:
+            for _ in range(lenB-lenA):
                 headB = headB.next
-            return headA
-                
-                    
+        
+        while headA != headB and (headA and headB):
+            headA = headA.next
+            headB = headB.next
+        return headA
