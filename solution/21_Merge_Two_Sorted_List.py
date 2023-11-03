4bd33556f649455ec0c14905708777be6aa76c05
CodeXplore <codexplore.channel@gmail.com>
2023-11-03 12:12:45 +0700

    Update 21_Merge_Two_Sorted_List.py

--- solution/21_Merge_Two_Sorted_List.py
+++ solution/21_Merge_Two_Sorted_List.py
@@ -1,26 +1,30 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        #dummy_head to keep track the head of the result linked list
-        #tail to keep track the end of the result linked list
-        
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
         """
-        Time Complexity: O(n+m)
-        Space: O(1) as re-use existing nodes
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
         """
+        # to create a reference to the result
         dummy_head = tail = ListNode()
+        # to ensure that both l1 and l2 is not None
         while (l1 and l2):
-            if l1.val < l2.val:
-                tail.next, l1 = l1, l1.next
+            if l1.val >= l2.val:
+                tail.next = l2
+                l2 = l2.next
             else:
-                tail.next, l2 = l2, l2.next
+                tail.next = l1
+                l1 = l1.next
+
             tail = tail.next
         
         if l1: tail.next = l1
         if l2: tail.next = l2
+
+        return dummy_head.next
         
-        return dummy_head.next #as the init dummy_head.val = 0
